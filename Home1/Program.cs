b64c41d4d9ad6c476a119c3d1d37b6f6f3d3ace2
алишер <aliser@MacBook-Air-aliser.local>
2023-03-08 04:15:17 -0800

    Add homework 1

---
+++ Home1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+int num = new Random().Next(100, 1000);
+int NumA = num/100;
+int NumB =(num/10)%10;
+int NumC = num%10;
+int result = NumA * 10 + NumC;
+Console.Write($"Исключили из числа {num} -> число {NumB} -> {result}");
+
+
