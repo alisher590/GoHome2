ad96914d7ecb17bd77e574d504a894f38df2ed64
алишер <aliser@MacBook-Air-aliser.local>
2023-03-08 07:45:27 -0800

    Add homework 3

---
+++ Home3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+void PrintDay(int num)
+{
+    if(num > 0 && num < 8)
+    {
+        if (num == 6 || num == 7) 
+        Console.WriteLine("Выходной");
+        else 
+        Console.WriteLine("Это рабочий день");
+    }
+    else Console.WriteLine("Такого дня несуществует!");
+}
+Console.Write("Введите число дня недели: ");
+int numberOfDay = Convert.ToInt32(Console.ReadLine());
+
+PrintDay(numberOfDay);
